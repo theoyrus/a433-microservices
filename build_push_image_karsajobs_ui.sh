75fdae731cd6ec16a64c460b5bce64b97bf1d977
Suryo Prasetyo W <the.oyrus@gmail.com>
2022-11-25 15:49:30 +0700

    tambah config circleci & shell script build image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,13 @@
+# Shell script untuk Build Docker Image dan push ke Github Container Registry
+
+# membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image karsajobs, dan memiliki tag latest
+docker build -t karsajobs-ui:latest .
+
+# tag image untuk penanda Github Container Registry
+docker tag karsajobs-ui:latest ghcr.io/theoyrus/karsajobs-ui:latest
+
+# authorize Github Container Registry menggunakan PAT
+echo $GHCR_PAT | docker login ghcr.io -u theoyrus --password-stdin
+
+# push ke GHCR
+docker push ghcr.io/theoyrus/karsajobs-ui:latest
